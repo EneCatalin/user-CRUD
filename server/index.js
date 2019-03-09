ad6cfed1f5abda89389f5289db0d5c2ec151c3f1
Chris <enecatalin9393@gmail.com>
2019-03-09 18:20:33 +0200

    had to save this, working on the crud part, got a little stuck, with some luck the next update will have the full create user feature. This is a save point. Anyway, back to it

--- server/index.js
+++ server/index.js
@@ -1,12 +1,78 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
 const app = express();
 
-mongoose.connect(keys.mongoURI);
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoutes = express.Router();
+const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+let User = require("./models/User");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(keys.mongoURI);
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
 });
 
-app.listen(5000);
+userRoutes.route("/").get(function(req, res) {
+  User.find(function(err, users) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(users);
+    }
+  });
+});
+
+userRoutes.route("/:id").get(function(req, res) {
+  let id = req.params.id;
+  User.findById(id, function(err, user) {
+    res.json(user);
+  });
+});
+
+userRoutes.route("/add").post(function(req, res) {
+  let user = new User(req.body);
+  user
+    .save()
+    .then(user => {
+      res.status(200).json({ user: "user added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new user failed" + err);
+    });
+});
+
+userRoutes.route("/update/:id").post(function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if (!user) {
+      res.status(404).send("data is not found");
+    } else {
+      user.id = req.body.id;
+      // user.user_name = req.body.user_name;
+      // user.user_username = req.body.user_username;
+      // user.user_email = req.body.user_email;
+      // user.user_phone = req.body.user_phone;
+      // user.user_website = req.body.user_website;
+
+      user
+        .save()
+        .then(user => {
+          res.json("User updated");
+        })
+        .catch(err => {
+          res.status(400).send("Update not possible" + err);
+        });
+    }
+  });
+});
+
+app.use("/users", userRoutes);
+
+app.listen(PORT);
--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,37 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 // const {Schema} = mongoose; same as const Schema=mongoose.Schema
 
-const userSchema = new Schema({
+let User = new Schema({
   id: {
     type: Number,
     required: [true, "Please enter an id"],
     unique: true
   },
   name: {
-    type: String,
-    required: [true, "Please enter a name"]
+    type: String
+    // required: [true, "Please enter a name"]
   },
   username: {
-    type: String,
-    required: [true, "Please enter a username"],
-    unique: true
+    type: String
+    // required: [true, "Please enter a username"],
+    // unique: true
   },
   email: {
-    type: String,
-    required: [true, "Please enter an email"],
-    unique: true
+    type: String
+    // required: [true, "Please enter an email"],
+    // unique: true
   },
   phone: {
-    type: Number,
-    required: [true, "Please enter a phone number"],
-    unique: true
+    type: String
+    // required: [true, "Please enter a phone number"],
+    // unique: true
   },
   website: {
-    type: String,
-    required: [true, "Please enter a website"],
-    unique: true
+    type: String
+    // required: [true, "Please enter a website"],
+    // unique: true
   }
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", User);
